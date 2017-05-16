917bf7151b719defd781238971e7faab7053e332
Daniel Jones <drjones18@gmail.com>
2017-05-16 13:02:16 -0400

    Align header content with main content.

--- wp-content/themes/logic-department/header.php
+++ wp-content/themes/logic-department/header.php
@@ -24,22 +24,24 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'logic-department' ); ?></a>
 
 	<header id="masthead" class="site-header <?php if ( is_user_logged_in() ) { echo "site-header--logged-in"; } ?>" role="banner">
-		<div class="site-branding">
-			<?php if ( get_header_image() ) : ?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-branding__logo-link" rel="home">
-					<img src="<?php esc_url( header_image() ); ?>"class="site-branding__logo-image">
-				</a>
-			<?php endif; // End header image check. ?>
-		</div><!-- .site-branding -->
+		<div class="header__inner">
+			<div class="site-branding">
+				<?php if ( get_header_image() ) : ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-branding__logo-link" rel="home">
+						<img src="<?php esc_url( header_image() ); ?>"class="site-branding__logo-image">
+					</a>
+				<?php endif; // End header image check. ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-				<span class="menu-toggle__line"></span>
-				<span class="menu-toggle__line"></span>
-				<span class="menu-toggle__line"></span>
-			</button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'primary-menu' , 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<span class="menu-toggle__line"></span>
+					<span class="menu-toggle__line"></span>
+					<span class="menu-toggle__line"></span>
+				</button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'primary-menu' , 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
